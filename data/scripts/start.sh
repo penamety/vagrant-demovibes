ed73b683ce80aa5b0eb8350332c938519fff3ce5
arabek <arabek@gmail.com>
2017-05-23 22:16:24 +0200

    fix startup sequence

--- data/scripts/start.sh
+++ data/scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
-/vagrant/scripts/start-uwsgi.sh >~/logs/start-uwsgi.log 2>&1 &
-/vagrant/scripts/start-sockulf.sh >~/logs/start-sockulf.log 2>&1 &
-sleep 2
-/vagrant/scripts/start-demosauce.sh >~/logs/start-demosauce.log 2>&1 &
+cd /vagrant/scripts
+./start-uwsgi.sh >~/logs/start-uwsgi.log 2>&1 &
+./start-sockulf.sh >~/logs/start-sockulf.log 2>&1 &
+sleep 5
+./start-demosauce.sh >~/logs/start-demosauce.log 2>&1 &
 exit 0

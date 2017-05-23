7c00b039d9f9e6e49e3b5140855d255768e3bf3d
arabek <arabek@gmail.com>
2017-05-24 00:44:19 +0200

    cleanup demosauce and sockulf scripts introduce nohup

--- data/scripts/start-demosauce.sh
+++ data/scripts/start-demosauce.sh
@@ -5,38 +5,7 @@ LOGPATH="/home/vagrant/logs"
 
 cd ${RUNPATH}
 
-check_running() {
-    if [ -z $(pidof demosauce | tr -t [:blank:] '') ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-start_demosauce() {
-
-    now=$(date "+[%D %T]")
-    echo "${now} Starting DemoSauce" >> ${LOGPATH}/demosauce.log
-    echo "${now} Starting DemoSauce" >> ${LOGPATH}/demosauce.err
-    ./demosauce -c demosauce.conf >> ${LOGPATH}/demosauce.log 2>> ${LOGPATH}/demosauce.err
-
-}
-
-do_check() {
-
-    RUNNING=$(check_running; echo $?)
-    NOW=$(date "+[%D %T]")
-
-    if [[ ${RUNNING} == 1 ]]; then
-        sleep 2
-    else
-        echo "${NOW} OOPS: demosauce might have crashed, taking over!" >> ${LOGPATH}/demosauce.log
-        start_demosauce
-    fi
-
-}
-
-while true; do
-    do_check
-    sleep 3
-done
+now=$(date "+[%D %T]")
+echo "${now} Starting DemoSauce" >> ${LOGPATH}/demosauce.log
+echo "${now} Starting DemoSauce" >> ${LOGPATH}/demosauce.err
+nohup ./demosauce -c demosauce.conf >${LOGPATH}/demosauce.log 2>${LOGPATH}/demosauce.err &
--- data/scripts/start-sockulf.sh
+++ data/scripts/start-sockulf.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
+. /home/vagrant/pyenv/bin/activate
+
 RUNPATH="/home/vagrant/demovibes/demovibes"
 LOGPATH="/home/vagrant/logs"
 
-cd "$RUNPATH"
+cd ${RUNPATH}
 
-# kill old instance
-pkill -9 sockulf.py
-
-. /home/vagrant/pyenv/bin/activate
-
-while true; do 
-	python sockulf.py >> "$LOGPATH/sockulf.log" 2>> "$LOGPATH/sockulf.err"
-	sleep 30
-done
+nohup python sockulf.py >"$LOGPATH/sockulf.log" 2>"$LOGPATH/sockulf.err"
